73594202f4ea5ec06db5ec612e3ece86166ea729
Dakilang-Tamad <59394084+Dakilang-Tamad@users.noreply.github.com>
2022-05-25 17:07:52 +0800

    Main feature update
    Finalized quiz algorithm

--- data_handling.py
+++ data_handling.py
@@ -1,8 +1,25 @@
 import sqlite3
+import random
 from pathlib import Path
 
-def new_quiz(base):
-    conn = sqlite3.connect("Quizzes.db")
-    conn.execute("CREATE TABLE CURRENT QUIZ (ID INT PRIMARY KEY, CATEGORY TEXT, ITEM INT);")
-    for i in range(3):
-        red = int
+
+def new_quiz():
+    v = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    k = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    g = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
+         26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+    quiz = []
+    for i in range(10):
+        if i in (0, 3, 6, 9):
+            quiz.append(g.pop(random.randrange(len(g))))
+        if i in (1, 4, 7):
+            quiz.append(v.pop(random.randrange(len(v))))
+        if i in (2, 5, 8):
+            quiz.append(k.pop(random.randrange(len(k))))
+
+    return quiz
+
+current = 0
+score = 0
+contents = new_quiz()
+level = "N5"
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import sqlite3
+from pathlib import Path
+from kivy.properties import StringProperty
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.text import LabelBase
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
+import data_handling as dh
 import time
+
 
 class GrammarPop(Popup):
     pass
@@ -21,7 +26,7 @@ class KanjiPop(Popup):
 class LoadingScreen(Screen):
     text = "日本語 Wakaranai"
     def on_enter(self, *args):
-        Clock.schedule_once(self.proceed, 3)
+        Clock.schedule_once(self.proceed, 1)
 
     def proceed(self, event):
         self.manager.current = "home"
@@ -32,7 +37,9 @@ class HomeScreen(Screen):
 
 
 class DifficultySelection(Screen):
-    pass
+    def start5(self):
+        dh.level = "N5"
+        self.manager.current = "grammari"
 
 
 class BookmarkCategories(Screen):
@@ -62,15 +69,164 @@ class Courses(Screen):
 
 
 class GrammarItem(Screen):
-    pass
+
+    question = StringProperty()
+    A = StringProperty()
+    B = StringProperty()
+    C = StringProperty()
+    D = StringProperty()
+    ans = StringProperty()
+
+    def on_pre_enter(self, *args):
+        conn = sqlite3.connect("Quizzes.db")
+        cursor = conn.cursor()
+        retrieve_query = "select * from " + dh.level + "_GRAMMAR where ID = " + str(dh.contents[dh.current])
+        cursor.execute(retrieve_query)
+        contents = cursor.fetchall()
+        for i in contents:
+            self.question = i[1]
+            self.A = i[2]
+            self.B = i[3]
+            self.C = i[4]
+            self.D = i[5]
+            self.ans = i[6]
+        cursor.close()
+
+
+    def ent_a(self):
+        dh.current += 1
+        if self.ans == 'a':
+            dh.score += 1
+        self.next_screen()
+
+    def ent_b(self):
+        dh.current += 1
+        if self.ans == 'b':
+            dh.score += 1
+        self.next_screen()
+
+    def ent_c(self):
+        dh.current += 1
+        if self.ans == 'c':
+            dh.score += 1
+        self.next_screen()
+
+    def ent_d(self):
+        dh.current += 1
+        if self.ans == 'd':
+            dh.score += 1
+        self.next_screen()
+
+    def next_screen(self):
+        if dh.current == 10:
+            self.manager.current = "end"
+        else:
+            self.manager.current = "vocabi"
 
 
 class VocabItem(Screen):
-    pass
+
+    en = StringProperty()
+    kanji = StringProperty()
+    A = StringProperty()
+    B = StringProperty()
+    C = StringProperty()
+    D = StringProperty()
+    ans = StringProperty()
+
+    def on_pre_enter(self, *args):
+        conn = sqlite3.connect("Quizzes.db")
+        cursor = conn.cursor()
+        retrieve_query = "select * from " + dh.level + "_VOCAB where ID = " + str(dh.contents[dh.current])
+        cursor.execute(retrieve_query)
+        contents = cursor.fetchall()
+        for i in contents:
+            self.en = i[1]
+            if i[2] != "":
+                self.kanji = i[2]
+            else:
+                self.kanji = "<no kanji>"
+            self.A = i[3]
+            self.B = i[4]
+            self.C = i[5]
+            self.D = i[6]
+            self.ans = i[7]
+        cursor.close()
+
+    def ent_a(self):
+        dh.current += 1
+        if self.ans == 'a':
+            dh.score += 1
+        self.manager.current = "kanjii"
+
+    def ent_b(self):
+        dh.current += 1
+        if self.ans == 'b':
+            dh.score += 1
+        self.manager.current = "kanjii"
+
+    def ent_c(self):
+        dh.current += 1
+        if self.ans == 'c':
+            dh.score += 1
+        self.manager.current = "kanjii"
+
+    def ent_d(self):
+        dh.current += 1
+        if self.ans == 'd':
+            dh.score += 1
+        self.manager.current = "kanjii"
 
 
 class KanjiItem(Screen):
-    pass
+
+    sentence = StringProperty()
+    kanji = StringProperty()
+    A = StringProperty()
+    B = StringProperty()
+    C = StringProperty()
+    D = StringProperty()
+    ans = StringProperty()
+
+    def on_pre_enter(self, *args):
+        conn = sqlite3.connect("Quizzes.db")
+        cursor = conn.cursor()
+        retrieve_query = "select * from " + dh.level + "_KANJI where ID = " + str(dh.contents[dh.current])
+        cursor.execute(retrieve_query)
+        contents = cursor.fetchall()
+        for i in contents:
+            self.sentence = i[1]
+            self.kanji = i[2]
+            self.A = i[3]
+            self.B = i[4]
+            self.C = i[5]
+            self.D = i[6]
+            self.ans = i[7]
+        cursor.close()
+
+    def ent_a(self):
+        dh.current += 1
+        if self.ans == 'a':
+            dh.score += 1
+        self.manager.current = "grammari"
+
+    def ent_b(self):
+        dh.current += 1
+        if self.ans == 'b':
+            dh.score += 1
+        self.manager.current = "grammari"
+
+    def ent_c(self):
+        dh.current += 1
+        if self.ans == 'c':
+            dh.score += 1
+        self.manager.current = "grammari"
+
+    def ent_d(self):
+        dh.current += 1
+        if self.ans == 'd':
+            dh.score += 1
+        self.manager.current = "grammari"
 
 
 class EndQuizz(Screen):
@@ -88,7 +244,19 @@ class EndQuizz(Screen):
 
 
 class WindowManager(ScreenManager):
-    pass
+    def launch(self, base):
+        qc = QuizCont()
+        if qc.current < 4:
+            qc.current += 1
+            self.current = "grammari"
+        if (qc.current > 3) and (qc.current < 7):
+            qc.current += 1
+            self.current = "vocabi"
+        if (qc.current > 6) and (qc.current < 10):
+            qc.current += 1
+            self.current = "kanjii"
+        else:
+            self.current = "end"
 
 
 kv = Builder.load_file("my.kv")
